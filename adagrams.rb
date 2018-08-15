7e3ec03ffa9b50793765bbfe075be4d20ff03759
Leanne Rivera <rivera.leanne+ada@gmail.com>
2018-08-15 15:45:56 -0700

    wave 2 part 1 done

--- adagrams.rb
+++ adagrams.rb
@@ -1,18 +1,20 @@
+#require 'pry'
+
 letters ={
-"A": -9,
-"B": -2,
+"A": 9,
+"B": 2,
 "C": 2,
 "D": 4,
-"E": 12,
-"F": 2,
-"G": 3,
-"H": 2,
-"I": 9,
-"J": 1,
-"K": 1,
-"L": 4,
-"M": 2,
-"N": 6,
+# "E": 12,
+# "F": 2,
+# "G": 3,
+# "H": 2,
+# "I": 9,
+# "J": 1,
+# "K": 1,
+# "L": 4,
+# "M": 2,
+# "N": 6,
 "O": 8,
 "P": 2,
 "Q": 1,
@@ -26,17 +28,30 @@ letters ={
 "Z": 1
 }
 
-#---------------------draw 10 letters-----------------
+#-------------WAVE-1--draw--10--letters-----------------
 def draw_letters(hash)
-  player_letters = []
+  player_letters = [] #will hold return value
     hash.each do |k, v|
-      if v > 0
-        player_letters << "#{k}"
+      if v > 0 #skips all keys with values < 0
+        player_letters << "#{k}" #pushes valid key to the array
       end
-      hash[k] = v - 1
+      hash[k] = v - 1 #then subtracts the value by one
     end
-  return player_letters.sample(10)
-end
+    return player_letters.sample(10) #will not duplicate letters maybe .map somewhere?
+  end
 
 letters = draw_letters(letters)
 #------------------------------------------------------
+#drawn_letters must match player's words
+practice_word = "catt"
+practice_true = ["c", "b", "a", "t", "h"]
+practice_false = ["d", "c", "a", "b"]
+
+#is user input included in hand?
+def uses_available_letters?(input, letters_in_hand)
+  input.upcase! #converts to upcase for comparison of in hand (all caps)
+  input = input.split("") #splits input into array of characters
+  check = input.all?{|i| letters_in_hand.include?(i)} #determines true or false
+end
+
+uses_available_letters?(practice_word, letters)
