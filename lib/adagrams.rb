f2c69f9748920841c8ddbe6c93c15ca2462d2add
Leanne Rivera <rivera.leanne+ada@gmail.com>
2018-08-17 15:26:47 -0700

    100% FINISHED with PASS

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -2,37 +2,37 @@ require 'pry'
 require 'awesome_print'
 
 # #-------------WAVE-1--PASS-----------------
-# def draw_letters
-#   #string of letters of the correct quantity
-#   letters = ("A" * 9) + ("B" * 2) + ("C" * 2) + ("D" * 4) + ("E" * 12) + ("F" * 2) + ("G" * 3) + ("H" * 2) + ("D" * 4) + ("E" * 12) + ("F" * 2) + ("G" * 3) + ("H" * 2) + ("I" * 9) + ("J" * 1) + ("K" * 1) + ("L" * 4) + ("M" * 2) + ("N" * 6) + ("O" * 8) + ("P" * 2) + ("Q" * 1) + ("R" * 6) + ("S" * 4) + ("T" * 6) + ("U" * 4) + ("V" * 2) + ("W" * 2) + ("Y" * 2) + ("Z" * 1)
-#   letters = letters.split(//) #turns string letters into array of every character
-#   # = letters.flatten.join()
-#   return letters.sample(10) #returns 10 elements of the array randomlys
-# end
+def draw_letters
+  #string of letters of the correct quantity
+  letters = ("A" * 9) + ("B" * 2) + ("C" * 2) + ("D" * 4) + ("E" * 12) + ("F" * 2) + ("G" * 3) + ("H" * 2) + ("D" * 4) + ("E" * 12) + ("F" * 2) + ("G" * 3) + ("H" * 2) + ("I" * 9) + ("J" * 1) + ("K" * 1) + ("L" * 4) + ("M" * 2) + ("N" * 6) + ("O" * 8) + ("P" * 2) + ("Q" * 1) + ("R" * 6) + ("S" * 4) + ("T" * 6) + ("U" * 4) + ("V" * 2) + ("W" * 2) + ("Y" * 2) + ("Z" * 1)
+  letters = letters.split(//) #turns string letters into array of every character
+  # = letters.flatten.join()
+  return letters.sample(10) #returns 10 elements of the array randomlys
+end
 #
 # # #-------------wave 2 PASS-------------------------
-# # drawn_letters must match player's words
-# #is user input included in hand?
-# #do not mess with this array (letters_in_hand)!!!!
-# def uses_available_letters?(input, letters_in_hand)
-#
-#   input = input.split("") #splits the string into array
-#   tru_fals = [] #array to determine validity of input later
-#   input.each do |let|
-#       if letters_in_hand.include?(let) #if the input letter matches a letter in hand
-#         tru_fals << letters_in_hand.delete_at(letters_in_hand.index(let)) #delete letter from the one in hand & push to tru_fals
-#       else
-#         tru_fals << false #if no match, pushes false
-#       end
-#     end
-#
-#   if tru_fals.include?(false) #if there is any false, automatic false
-#     return false
-#   else
-#     return true #else input is valid
-#   end
-# end
-#
+# drawn_letters must match player's words
+#is user input included in hand?
+#do not mess with this array (letters_in_hand)!!!!
+def uses_available_letters?(input, letters_in_hand)
+
+  input = input.split("") #splits the string into array
+  tru_fals = [] #array to determine validity of input later
+  input.each do |let|
+      if letters_in_hand.include?(let) #if the input letter matches a letter in hand
+        tru_fals << letters_in_hand.delete_at(letters_in_hand.index(let)) #delete letter from the one in hand & push to tru_fals
+      else
+        tru_fals << false #if no match, pushes false
+      end
+    end
+
+  if tru_fals.include?(false) #if there is any false, automatic false
+    return false
+  else
+    return true #else input is valid
+  end
+end
+
 # # #-------------------wave 3--PASS----------------
 def score_word(word)
   word = word.upcase.split("") #to array of strings
@@ -73,7 +73,7 @@ def highest_score_from_words(words)
     word = word.to_s #turns the new array into a string
     score = score_word(word)
 
-    if score == winner[:score]
+    if score == winner[:score] #only exceptions are ties
 
       if winner[:word].length == 10
         winner[:word] = winner[:word]
--- specs/adagrams_spec.rb
+++ specs/adagrams_spec.rb
@@ -8,78 +8,78 @@ require_relative '../lib/adagrams'
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 describe 'Adagrams' do
-  # describe 'draw_letters method' do
-  #   it 'draws ten letters from the letter pool' do
-  #     drawn_letters = draw_letters
-  #     expect(drawn_letters.size).must_equal 10
-  #   end
-  #
-  #   it 'returns an array, and each item is a single-letter string' do
-  #     drawn_letters = draw_letters
-  #     expect(drawn_letters.size).must_equal 10
-  #
-  #     expect(drawn_letters).must_be_instance_of Array
-  #     drawn_letters.each do |letter|
-  #       expect(letter).must_be_instance_of String
-  #       expect(letter.length).must_equal 1
-  #     end
-  #   end
-  # end
+  describe 'draw_letters method' do
+    it 'draws ten letters from the letter pool' do
+      drawn_letters = draw_letters
+      expect(drawn_letters.size).must_equal 10
+    end
 
-  # describe 'uses_available_letters? method' do
-  #
-  #   it 'returns true if the submitted letters are valid against the drawn letters' do
-  #     drawn_letters = ['D', 'O', 'G', 'X', 'X', 'X', 'X', 'X', 'X', 'X']
-  #     test_word = 'DOG'
-  #
-  #     is_valid = uses_available_letters? test_word, drawn_letters
-  #
-  #     expect(is_valid).must_equal true
-  #   end
-  #
-  #   it 'returns false word contains letters not in the drawn letters' do
-  #     drawn_letters = ['D', 'O', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X']
-  #     test_word = 'DOG'
-  #
-  #     is_valid = uses_available_letters? test_word, drawn_letters
-  #
-  #     expect(is_valid).must_equal false
-  #   end
-  #
-  #   it 'returns false word contains repeated letters more than in the drawn letters' do
-  #     drawn_letters = ['A', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X']
-  #     test_word = 'AAA'
-  #
-  #     is_valid = uses_available_letters? test_word, drawn_letters
-  #
-  #     expect(is_valid).must_equal false
-  #   end
-  #
-  # end
+    it 'returns an array, and each item is a single-letter string' do
+      drawn_letters = draw_letters
+      expect(drawn_letters.size).must_equal 10
 
-  # describe 'score_word method' do
-  #   it 'returns an accurate numerical score according to the score chart' do
-  #     expect(score_word("A")).must_equal 1
-  #     expect(score_word("DOG")).must_equal 5
-  #     expect(score_word("WHIMSY")).must_equal 17
-  #   end
-  #
-  #   it 'returns a score regardless of input case' do
-  #     expect(score_word("a")).must_equal 1
-  #     expect(score_word("dog")).must_equal 5
-  #     expect(score_word("wHiMsY")).must_equal 17
-  #   end
-  #
-  #   it 'returns a score of 0 if given an empty input' do
-  #     expect(score_word("")).must_equal 0
-  #   end
-  #
-  #   it 'adds an extra 8 points if the word is 7 or more characters long' do
-  #     expect(score_word("XXXXXXX")).must_equal 64
-  #     expect(score_word("XXXXXXXX")).must_equal 72
-  #     expect(score_word("XXXXXXXXX")).must_equal 80
-  #   end
-  # end
+      expect(drawn_letters).must_be_instance_of Array
+      drawn_letters.each do |letter|
+        expect(letter).must_be_instance_of String
+        expect(letter.length).must_equal 1
+      end
+    end
+  end
+
+  describe 'uses_available_letters? method' do
+
+    it 'returns true if the submitted letters are valid against the drawn letters' do
+      drawn_letters = ['D', 'O', 'G', 'X', 'X', 'X', 'X', 'X', 'X', 'X']
+      test_word = 'DOG'
+
+      is_valid = uses_available_letters? test_word, drawn_letters
+
+      expect(is_valid).must_equal true
+    end
+
+    it 'returns false word contains letters not in the drawn letters' do
+      drawn_letters = ['D', 'O', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X']
+      test_word = 'DOG'
+
+      is_valid = uses_available_letters? test_word, drawn_letters
+
+      expect(is_valid).must_equal false
+    end
+
+    it 'returns false word contains repeated letters more than in the drawn letters' do
+      drawn_letters = ['A', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X']
+      test_word = 'AAA'
+
+      is_valid = uses_available_letters? test_word, drawn_letters
+
+      expect(is_valid).must_equal false
+    end
+
+  end
+
+  describe 'score_word method' do
+    it 'returns an accurate numerical score according to the score chart' do
+      expect(score_word("A")).must_equal 1
+      expect(score_word("DOG")).must_equal 5
+      expect(score_word("WHIMSY")).must_equal 17
+    end
+
+    it 'returns a score regardless of input case' do
+      expect(score_word("a")).must_equal 1
+      expect(score_word("dog")).must_equal 5
+      expect(score_word("wHiMsY")).must_equal 17
+    end
+
+    it 'returns a score of 0 if given an empty input' do
+      expect(score_word("")).must_equal 0
+    end
+
+    it 'adds an extra 8 points if the word is 7 or more characters long' do
+      expect(score_word("XXXXXXX")).must_equal 64
+      expect(score_word("XXXXXXXX")).must_equal 72
+      expect(score_word("XXXXXXXXX")).must_equal 80
+    end
+  end
   #
   describe 'highest_score_from method' do
     it 'returns a hash that contains the word and score of best word in an array' do
